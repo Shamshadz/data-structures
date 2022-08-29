38a1a6d1a4f0e883b03de9807b6e88ca72280ede
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2022-08-29 09:43:46 +0530

    fix

--- linkedlist/display_linklist.cpp
+++ linkedlist/display_linklist.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class node{
+class node
+{
 public:
     int data;
     node *next;
-}*first =NULL;
+} *first = NULL;
 
-void create(int A[], int n){
+void create(int A[], int n)
+{
     int i;
     node *t, *last;
     first = new node;
@@ -15,29 +17,32 @@ void create(int A[], int n){
     first->next = NULL;
     last = first;
 
-    for(i=1;i<n;i++){
+    for (i = 1; i < n; i++)
+    {
         t = new node;
         t->data = A[i];
         t->next = NULL;
-        last->next=t;
-        last=t;
-    }
-
-};
-
-// void display(node *p){
-//     while(p!=NULL){
-//         cout<<p->data<<endl;
-//         p=p->next;
-//     }
-// }
-
-void RDisplay(node *p){
-    if(p!=NULL){
-        cout<<p->data<<" ";
-        RDisplay(p->next);
+        last->next = t;
+        last = t;
     }
 }
+
+void display(node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+// void RDisplay(node *p){
+//     if(p!=NULL){
+//         cout<<p->data<<" ";
+//         RDisplay(p->next);
+//     }
+// }
 
 // int count(node *p){
 //     int c=0;
@@ -48,12 +53,12 @@ void RDisplay(node *p){
 //     return (c);
 // }
 
-// int count (node *p){      // recursive method
-//     if(p==0){
-//         return 0;          // O(n) - Time
-//     }                     // O(n) - Space
-//     return 1 + count(p->next);
-// }
+int count (node *p){      // recursive method
+    if(p==0){
+        return 0;          // O(n) - Time
+    }                     // O(n) - Space
+    return 1 + count(p->next);
+}
 
 // int Add(node *p){          // recursive O(n), O(n)
 //     int sum = 0;
@@ -62,7 +67,7 @@ void RDisplay(node *p){
 //     }
 //     else{
 //     sum += p->data;
-//     return Add(p->next) + sum; 
+//     return Add(p->next) + sum;
 //     }
 // }
 
@@ -91,19 +96,88 @@ void RDisplay(node *p){
 //     }
 // }
 
-int main() {
+// node *LSearch(node *p, int key) 
+// {
 
-    int A[]={2,4,54,65,21};
+//     node *q = NULL;
 
-    create(A,5);
+//     while (p != NULL)
+//     {
+//         if (key == p->data)
+//         {
+//             q->next = p->next;
+//             p->next = first;
+//             first = p;
+//             return p;
+//         }
+//         q = p;
+//         p = p->next;
+//     }
+//     return NULL;
+// }
 
-    // display(first);
-    RDisplay(first);
+// node *RSearch(node *p, int key)
+// {
+//     if (p == NULL)
+//     {
+//         return NULL;
+//     }
+//     if (p->data = key)
+//     {
+//         return p;
+//     }
+//     return RSearch(p->next, key);
+// }
+
+void Insert(node *p, int pos, int x)
+{
+    node *t = new node;
+
+    if(pos < 0 || pos > count(p))
+        return;
+        
+    t->data = x;
+    t->next = NULL;
+    
+
+    if (pos == 0)
+    {
+        t->next = first;
+        first = t;
+    }
+    else
+    {
+        for (int i = 0; i < pos-1; i++)
+        {
+            p = p->next;
+        }
+            t->next = p->next;
+            p->next = t;
+    }
+}
+
+int main()
+{
+
+    int A[] = {2, 4, 54, 65, 21};
+    node *temp = NULL;
+
+    create(A, 5);
+
+    display(first);
+    // RDisplay(first);
     // count(first);
     // cout<<Add(first);
     // cout<<max(first);
 
-    
+    // temp = LSearch(first, 21);
+    // if (temp)
+    //     cout<<"Key is found "<<temp->data<<endl;
+    // else
+    //     cout<<"Key not found"<<endl;
 
-   return 0;
+    Insert(first, 3, 97);
+    display(first);
+
+    return 0;
 }
