28ef37ed6ff5a7f4c9f54ebd432ecbdb4ee457c1
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-01-30 12:25:21 +0530

    update

--- leetcode/largestSubString.java
+++ leetcode/largestSubString.java
@@ -4,13 +4,13 @@ public class largestSubString
 {
 	public static void main(String[] args) {
 
-        String s = "abcabcdab";
+        String s = "dvdf";
         Stack<Character> stack = new Stack<>();
         String cmp1="";
         String cmp2="";
         // System.out.println(s.length());
         for(int i=0;i<s.length();i++){
-            // System.out.println(stack.contains(s.charAt(i)));
+            System.out.println(s.charAt(i));
             if(stack.contains(s.charAt(i))){
                 // cmp2 += s.charAt(i);
                 while(!stack.isEmpty()){
@@ -26,9 +26,18 @@ public class largestSubString
                 cmp2 = "";
             }else{
                 stack.push(s.charAt(i));
-                cmp1+=s.charAt(i);
+                // cmp1+=s.charAt(i);
             }
         }
+
+        while(!stack.isEmpty()){
+            cmp2 += stack.pop();
+        }
+        if(cmp1.length()<cmp2.length()){
+            cmp1 = cmp2;
+        }
+        
+        
         System.out.println(cmp1);
     }
 }
