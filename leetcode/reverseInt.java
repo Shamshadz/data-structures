893804c31a99ac2f9774e7eee4ed952d78a9fb5a
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-01-30 18:41:31 +0530

    update

---
+++ leetcode/reverseInt.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class reverseInt {
+
+    static int reverseStr(String n){
+
+		Stack<Character> stack = new Stack<Character>();
+		String s= "";
+		
+		int len = n.length();
+		for(int i=0;i<len;i++){
+		    stack.push(n.charAt(i));
+		}
+		while(!stack.empty()){
+		    s += stack.pop().toString();
+		}
+		int ans = Integer.parseInt(s);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        
+        int x = -321;
+		
+        if(x<0){
+            x = -x;
+            String n = Integer.toString(x);
+            int ans = reverseStr(n);
+            System.out.println(-ans);
+        }
+        else{
+            String n = Integer.toString(x);
+            int ans = reverseStr(n);
+            System.out.println(ans);
+        }
+		
+		
+
+    }
+}
